293d01cf9f911f56a31e81a9ee94736732142571
TonyWillianFIAP <rm550667@fiap.com.br>
2023-11-22 14:12:48 -0300

    Update rodapé

--- FRONTEND/src/components/Rodape/index.jsx
+++ FRONTEND/src/components/Rodape/index.jsx
@@ -3,6 +3,6 @@ import './rodape.scss';
 
 export default function Rodape() {
   return (
-    <footer className="rodape">© Desenvolvido pela FIAP</footer>
+    <footer className="rodape">© Desenvolvido por alunos da FIAP</footer>
   );
 }
