aa0d21d2dda7b894ebb9210838208a6db27f2bc5
rickparra <hpbenitez7@gmail.com>
2023-11-22 10:42:49 -0300

    arrumando pagina predict

--- FRONTEND/src/routes/Predict/index.jsx
+++ FRONTEND/src/routes/Predict/index.jsx
@@ -6,16 +6,18 @@ export default function App() {
   const [imageUrl, setImageUrl] = useState(null);
   const [result, setResult] = useState(null);
   const [username, setUsername] = useState('');
+  const [inputMessage, setInputMessage] = useState('');
+  const [chatMessages, setChatMessages] = useState([]);
 
   useEffect(() => {
     // Recupera o nome do usuário da sessionStorage
-    const storedUsername = sessionStorage.getItem("token-user");
+    const storedUsername = sessionStorage.getItem('token-user');
     if (storedUsername) {
       setUsername(storedUsername);
     }
   }, []);
 
-  const handleImageChange = (e) => {
+  const handleImageChange = async (e) => {
     const uploadedImage = e.target.files[0];
 
     // Atualiza a URL da imagem para visualização instantânea
@@ -24,8 +26,11 @@ export default function App() {
     // Atualiza o estado da imagem
     setImage(uploadedImage);
 
+    // Adiciona a mensagem da imagem ao chat
+    setChatMessages((prevMessages) => [...prevMessages, { type: 'user-image', image: uploadedImage, text: '' }]);
+
     // Realiza a previsão automaticamente
-    handlePrediction(uploadedImage);
+    await handlePrediction(uploadedImage);
   };
 
   const handlePrediction = async (image) => {
@@ -46,60 +51,88 @@ export default function App() {
     const data = await response.json();
     setResult(data);
 
-    console.log(data);
+    // Adiciona a mensagem do bot ao chat com o resultado
+    setChatMessages((prevMessages) => [
+      ...prevMessages,
+      { type: 'bot', text: `A classe prevista é ${getClassName(data.predicted_class)}` },
+    ]);
   };
 
-  if (sessionStorage.getItem("token-user")) {
+  const getClassName = (predictedClass) => {
+    switch (predictedClass) {
+      case 0:
+        return 'glioma';
+      case 1:
+        return 'meningioma';
+      case 2:
+        return 'sem tumor';
+      case 3:
+        return 'pituitary';
+      default:
+        return '';
+    }
+  };
+
+  const handleSendMessage = () => {
+    if (inputMessage.trim() === '') return;
+
+    // Adiciona a mensagem do usuário ao estado de mensagens do chat
+    setChatMessages((prevMessages) => [...prevMessages, { type: 'user-text', text: inputMessage, image: null }]);
+    setInputMessage('');
+
+    // Adicione a lógica de envio da mensagem para o chatbot aqui (se necessário)
+  };
+
+  const handleKeyDown = (event) => {
+    if(event.key === 'Enter') {
+      handleSendMessage();
+    }
+  }
+
+  if (sessionStorage.getItem('token-user')) {
     return (
       <div>
         <div className="container">
           <div className="presentation">
-            Olá {username}, como posso te ajudar?
+            <h1>Olá {username}, como posso te ajudar?</h1>
           </div>
-          <form>
-            <label htmlFor="imageInput" className="upload-container">
-              Arraste e solte ou clique aqui para escolher uma imagem
+          <div className="chat-container">
+            {chatMessages.map((message, index) => (
+              <div key={index} className={message.type === 'user-text' ? 'user-message' : message.type === 'bot' ? 'bot-message' : 'image-message'}>
+                {message.type === 'user-text' && <div className="user-message-content">{message.text}</div>}
+                {message.type === 'bot' && <div className="bot-message-content">{message.text}</div>}
+                {message.type === 'user-image' && <img src={URL.createObjectURL(message.image)} alt="User Uploaded" className="uploaded-image" />}
+              </div>
+            ))}
+          </div>
+          <div className="inputs-chat">
+            <div className="input-container">
               <input
-                id="imageInput"
-                type="file"
-                onChange={(e) => handleImageChange(e)}
-                className="upload-input"
+                value={inputMessage}
+                onChange={(e) => setInputMessage(e.target.value)}
+                onKeyDown={handleKeyDown}
+                placeholder="Digite sua mensagem..."
               />
-            </label>
-
-            {imageUrl && (
-              <div className="image-container">
-                <img
-                  src={imageUrl}
-                  alt="Uploaded"
-                  className="uploaded-image"
+              <button onClick={handleSendMessage}>Enviar</button>
+            </div>
+            <form>
+              <label htmlFor="imageInput" className="upload-container">
+                Arraste e solte ou clique aqui para escolher uma imagem
+                <input
+                  id="imageInput"
+                  type="file"
+                  onChange={(e) => handleImageChange(e)}
+                  className="upload-input"
                 />
-              </div>
-            )}
-
-            {result && (
-              <div className="result">
-                Resultado: A classe predita é{' '}
-                {result.predicted_class === 0
-                  ? 'glioma'
-                  : result.predicted_class === 1
-                    ? 'meningioma'
-                    : result.predicted_class === 2
-                      ? 'sem tumor'
-                      : result.predicted_class === 3
-                        ? 'pituitary'
-                        : ''}
-              </div>
-            )}
-          </form>
+              </label>
+            </form>
+          </div>
         </div>
-        <div className="container_2">
-
-        </div>
+        <div className="container_2"></div>
       </div>
     );
   } else {
-    window.location.href = "/";
+    window.location.href = '/';
     return null; // Adicione um retorno nulo para evitar problemas de renderização
   }
 }
